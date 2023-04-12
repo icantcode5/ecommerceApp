f38bfb859e922d3512486edfd6e7a45382a38205
Carlos Sosa <carlossosa54321@gmail.com>
2023-04-12 03:47:41 -0700

    testing stripe redirect url

--- routes/payment.js
+++ routes/payment.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const paymentController = require("../controllers/payment")
 
 router.post("/create", paymentController.checkoutCart)
+router.get("/success", paymentController.getSuccessPage)
 
 module.exports = router
